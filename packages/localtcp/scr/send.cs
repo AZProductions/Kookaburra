22f1b8724bf1e4ae356ec774a3ba8ed17f44772c
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2021-04-23 20:32:12 +0200

    Update send.cs

--- packages/localtcp/scr/send.cs
+++ packages/localtcp/scr/send.cs
@@ -1,1 +1,56 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading;
 
+            //Code starts here.
+            //AZ Productions 2021
+            void sendfile(string ip) 
+            {
+                try
+                {
+                    IPAddress ipAd = IPAddress.Parse("127.0.0.1");
+                    // use local m/c IP address, and 
+                    // use the same in the client
+
+                    /* Initializes the Listener */
+                    TcpListener myList = new TcpListener(ipAd, 8001);
+
+                    /* Start Listeneting at the specified port */
+                    myList.Start();
+
+                    Console.WriteLine("The server is running at port 8001...");
+                    Console.WriteLine("The local End point is  :" +
+                                      myList.LocalEndpoint);
+                    Console.WriteLine("Waiting for a connection.....");
+
+                    Socket s = myList.AcceptSocket();
+                    Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+                    byte[] b = new byte[100];
+                    int k = s.Receive(b);
+                    Console.WriteLine("Recieved...");
+                    for (int i = 0; i < k; i++)
+                        Console.Write(Convert.ToChar(b[i]));
+
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    s.Send(asen.GetBytes("The string was recieved by the server."));
+                    Console.WriteLine("\nSent Acknowledgement");
+                    /* clean up */
+                    s.Close();
+                    myList.Stop();
+
+                    Thread.Sleep(1000);
+                    Console.WriteLine("");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error..... " + e.StackTrace);
+                }
+            }
