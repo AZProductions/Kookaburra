92e1d738ddddc370c72157c7e3e483fd834145c9
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2021-07-16 15:43:55 +0200

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -91,7 +91,7 @@ namespace KookaburraShell
                         .Color(Color.Green));
 
                     Console.WriteLine(Environment.NewLine);
-
+                    
                     var rule1 = new Rule("[white bold]All CLI  Commands[/]");
                     AnsiConsole.Render(rule1);
 
@@ -101,7 +101,6 @@ namespace KookaburraShell
                     table.AddColumn("Command");
                     table.AddColumn(new TableColumn("Description").Centered());
                     table.AddRow("cp", "[green]Copy files from one place to another.[/]");
-                    table.AddRow("clear [blue]or[/] cls", "[green]Copy files from one place to another.[/]");
                     table.AddRow("rm", "[green]Delete files.[/]");
                     table.AddRow("rmdir", "[green]Delete folders.[/]");
                     table.AddRow("mkdir", "[green]Make folders.[/]");
@@ -863,7 +862,7 @@ namespace KookaburraShell
                     }
                     else if (loc.EndsWith(".kookaburra"))
                     {
-
+                        
                     }
                     else
                     {
@@ -999,7 +998,7 @@ namespace KookaburraShell
                     int rand = random.Next(1, 10);
 
                     //New tips coming soon..
-                    switch (rand)
+                    switch (rand) 
                     {
                         case 1:
                             table.AddRow("[green bold]You can customzie the startup location by edeting the startup_loc.txt file.[/]");
@@ -1032,7 +1031,7 @@ namespace KookaburraShell
                             table.AddRow("[green bold]You can customzie the startup location by edeting the startup_loc.txt file[/]");
                             break;
                     }
-
+                    
                     table.Border(TableBorder.Heavy);
                     AnsiConsole.Render(table);
                 }
@@ -1586,7 +1585,6 @@ namespace KookaburraShell
                     var Intname = new[] { "" };
                     List<string> Stringname = new List<string>();
                     List<string> Stringvalue = new List<string>();
-                    BarChart bc = new BarChart();
                     string debugoff = File.ReadLines(file.ToString()).First();
                     if (debugoff == "app.debug-off") { } else { Debuger(); }
                     Isettingsconf.Quietmode = true;
@@ -1864,55 +1862,6 @@ namespace KookaburraShell
                                         string result = s.Replace("rm ", "");
                                         File.Delete(Format(result));
                                         script_var.Ifcount = 1;
-                                    }
-                                    else if (s.StartsWith("new Barchart"))
-                                    {
-                                        string result1 = s.Replace("new Barchart", "");
-                                        string result2 = result1.Replace(")", "");
-                                        result2 = result2.Replace("(", "");
-
-                                        bc.Width = 100;
-                                        bc.Label("[" + Console.ForegroundColor + "]" + Format(result2) + "[/]");
-                                        bc.CenterLabel();
-                                        script_var.Ifcount = 1;
-                                    }
-                                    else if (s.StartsWith("new Rule("))
-                                    {
-                                        string result = s.Replace("new Rule(", "");
-                                        result = result.Replace(")", "");
-                                        if (string.IsNullOrWhiteSpace(result))
-                                        {
-                                            var rule = new Rule("[" + Console.ForegroundColor + "][/]");
-                                            AnsiConsole.Render(rule);
-                                        }
-                                        else
-                                        {
-                                            var rule = new Rule("[" + Console.ForegroundColor + "]" + Format(result) + "[/]");
-                                            AnsiConsole.Render(rule);
-                                        }
-                                        script_var.Ifcount = 1;
-                                    }
-                                    else if (s.StartsWith("new Calendar(")) 
-                                    {
-                                        string result = s.Replace("new Calendar(", "");
-                                        result = result.Replace(")", "");
-                                        string[] valuearray = result.Split(", ");
-                                        var calendar = new Calendar(int.Parse(valuearray[0]), int.Parse(valuearray[1]));
-                                        calendar.HeaderStyle(Style.Parse(Console.ForegroundColor.ToString()));
-                                        AnsiConsole.Render(calendar);
-
-                                    }
-                                    else if (s.StartsWith("Barchart.Add(") && s.EndsWith(")"))
-                                    {
-                                        string result = s.Replace("Barchart.Add(", "");
-                                        result = result.Replace(")", "");
-                                        string[] valuearray = result.Split(", ");
-                                        bc.AddItem("[" + Console.ForegroundColor + "]" + Format(valuearray[0]) + "[/]" /*+ Format(valuearray[0])*/, Double.Parse(valuearray[1]), Console.ForegroundColor);
-                                        script_var.Ifcount = 1;
-                                    }
-                                    else if (s.StartsWith("Barchart.Display(") && s.EndsWith(")"))
-                                    {
-                                        AnsiConsole.Render(bc);
                                     }
                                     else if (s.StartsWith("zip "))
                                     {
