f19deb64bfe60033f782063c5874f782e43d7b70
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2021-08-16 15:24:37 +0200

    Updated `KookaburraEditor`.

--- lib/TextEditor/Program.cs
+++ lib/TextEditor/Program.cs
@@ -5,10 +5,9 @@ using System.IO;
 using System.Threading;
 
 class KookaburraEditor {
+	static string loc;
 	public static void Main(string[] args)
 	{
-
-
 		Application.Init();
 		var top = Application.Top;
 
@@ -28,24 +27,63 @@ class KookaburraEditor {
 			Y = Pos.Percent(0),
 			Width = Dim.Percent(100),
 			Height = Dim.Percent(100),
+			AllowsTab = true
 			//Text = File.ReadAllText(@"D:\Projects\Kookaburra\updates.txt")
 		};
 
 		var rect = new Rect(10, 20, 10, 20);
 
-		var fileDialog = new SaveDialog(/*"Save as", "Save", "Directory", "File name", "Do you want to save the file?"*/);
 
 		var menu = new MenuBar(new MenuBarItem[] {
 			new MenuBarItem ("_File", new MenuItem [] {
 				new MenuItem ("_New", "", () => { textView.Text = ""; /*Resets the text*/ }),
-				new MenuItem ("Save _As", "",() => { win.Add(fileDialog); /*The program closes for no reason.*/}),
-				new MenuItem ("_Open file", "",() => { /*todo: openfiledialog?*/ }),
+				new MenuItem ("Save _As", "",() => 
+				{
+					bool run = true;
+					while (run)
+					{
+						OpenDialog selector = new OpenDialog("Save File.", "Save the file currently open in the editor.")
+						{
+							CanChooseFiles = true,
+							AllowsMultipleSelection = false
+						};
+						Application.Run(selector);
+						if (selector.Canceled)
+							break;
+						else if (selector.FilePath != null)
+						{
+							File.WriteAllText(loc, textView.Text.ToString());
+							run = false;
+						}
+					}
+				}),
+				new MenuItem ("_Open file", "",() => 
+				{
+					bool run = true;
+					while (run)
+					{
+						OpenDialog selector = new OpenDialog("Select file.", "Select a file to edit.")
+						{
+							CanChooseFiles = true,
+							AllowsMultipleSelection = false
+						};
+						Application.Run(selector);
+						if (selector.Canceled)
+							break;
+						else if (selector.FilePath != null)
+						{
+							loc = selector.FilePath.ToString();
+							textView.Text = File.ReadAllText(selector.FilePath.ToString());
+							run = false;
+						}
+					}
+				}),
 				new MenuItem ("_Quit", "", () => { if (Quit ()) top.Running = false; })
 			}),
 			new MenuBarItem ("_Edit", new MenuItem [] {
-				new MenuItem ("_Copy", "", null),
-				new MenuItem ("C_ut", "", null),
-				new MenuItem ("_Paste", "", null)
+				new MenuItem ("_Copy", "", () => { textView.Copy(); }),
+				new MenuItem ("C_ut", "", () => { textView.Cut(); }/*,null, null, Key.End*/),
+				new MenuItem ("_Paste", "", () => { textView.Paste(); })
 			})
 		});
 		top.Add(menu);
@@ -56,6 +94,14 @@ class KookaburraEditor {
 			return n == 0;
 		}
 
+		/*textView.KeyPress += (k) =>
+		{
+			if (k.KeyEvent.IsCtrl) 
+			{
+				if (k.KeyEvent.Key == Key.A)
+					textView.SelectAll();
+			}
+		};*/
 
 		win.Add(
 			textView
