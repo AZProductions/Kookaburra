1f4dbff86ebfa3129473943cdbbe4088d569fd76
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2021-08-17 17:39:44 +0200

    Update check_environment.cs

--- src/check_environment.cs
+++ src/check_environment.cs
@@ -2,9 +2,9 @@
 {
     class check_environment
     {
-        public static bool Is64x { get; set; }
-        public static string Username { get; set; }
-        public static string OSversion { get; set; }
-        public static bool Debugoff { get; set; }
+        public static bool Is64x { get; set; } //Checks if pc is 64 based.
+        public static string Username { get; set; } //Checks the current username.
+        public static string OSversion { get; set; } //Gets the OSVersion.
+        public static bool Debugoff { get; set; } //Disable DebugMode, used when calling 'app.debug-off'.
     }
 }
